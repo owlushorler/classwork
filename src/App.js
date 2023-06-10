cd166e6af5744fcd3492de9f7032cf5cf47f85f2
Ajewole Gabriel <Ajewole.G@bullseye.local>
2023-06-10 07:07:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,80 @@
 import './style.css';
 import bg from './image/bg-header-desktop.svg'
-import Data from './data';
+import data from './data';
 import { useState } from 'react';
 
 
 
 function App() {
-  const [lo,setlo]=useState('')
+  const [lo,setlo]=useState('llll')
+
+ 
+
+ let pop = data
+
+ 
+let ap = pop.map(ele=>{
+  return(
+    <nav className='game' >
+  <div className='pal' >
+    <section>{ele.img}</section>
+    
+    <div className='cover' >
+       <section className='pa' >
+      <div>{ele.name}</div>
+      <div className='pb'>{ele.book}</div>
+      <div className='pbb'>  {ele.cat} </div>
+       </section>
+
+      <p  className='sen'  ><b> {ele.sen} </b></p>
+      <p className='se' > {ele.day}   <span className='span' > {ele.full}   </span> <span className='span' > {ele.usa}    </span>     </p>
+    </div>
+
+    <section className='lastbutton' >
+    <div className='button' >{ele.front}</div>
+    <div className='button' >{ele.senior}</div>
+    <div className='button' >{ele.Html}</div>
+    <div className='button' >{ele.css}</div>
+    <div className='button' >{ele.jav}</div>
+    </section>
+  </div>
+</nav>)
+  
+
+
+ })
+
+
+ function fil(){
+ let popp=  pop.filter(ele=>ele.id===1)
+  setlo(popp)
+ }
+
 
 
   return (
-    
+  
+
+   
   <div className='back' >
     <div  >
     <img className='head'  src= {bg} alt=""/>
 
 
-    <nav>
-      <div className='pal' >
-        <section>{Data[0].img}</section>
-        
-        <div className='cover' >
-           <section className='pa' >
-          <div>{Data.name}</div>
-          <div><button className='pb' >{Data[0].cat}</button></div>
-          <div><button className='pbb' > {Data[0].name} </button> </div>
-           </section>
-
-          <p  className='sen'  ><b> senior Frontend Devaloper </b></p>
-          <p className='se' > 1d ago     <span className='span' > . Part Time   </span> <span className='span' > . Usa only    </span>     </p>
-        </div>
-
-        <section className='lastbutton' >
-        <button className='button' >Frontend</button>
-        <button className='button' >senior</button>
-        <button className='button' >HTML</button>
-        <button className='button' >CSS</button>
-        <button className='button' >Javascript</button>
-
-        </section>
-      </div>
-    </nav>
-      
+    <div className='bbb' >
+      {ap}
     </div>
 
+      <button onClick={fil} >l;ol</button>
+      {lo}
+    
+
+    </div>
+
+   
+
   </div>
+   
   );
 }
 
--- src/data.js
+++ src/data.js
@@ -1,10 +1,19 @@
 import one from './image/photosnap.svg'
+import two from './image/manage.svg'
+import the from './image/account.svg'
+import four from './image/myhome.svg'
+import five from './image/loop-studios.svg'
+import six from './image/faceit.svg'
+import seven from './image/shortly.svg'
+import eig from './image/insure.svg'
+import nig from './image/eyecam-co.svg'
+import ten from './image/the-air-filter-company.svg'
 
-const Data =[{
+const data =[{
     id:1,
     img:<img src={one} alt=""></img>,
     name:"photosnap",
-    book:"New",
+    book:"!New",
     cat:"FEATURES",
     sen:"Senior frontend Developer",
     day:"1day ago ",
@@ -17,6 +26,143 @@ const Data =[{
     jav:"javascript"
 
     
-}]
+},
+{
+    id:2,
+    img:<img src={two} alt=""></img>,
+    name:"manage",
+    book:"!New",
+    cat:"FEATURES",
+    sen:"Full Developer",
+    day:"1day ago ",
+    full:"full time",
+    usa:"Remote",
+    front:"fullstack",
+    senior:"midwiegt",
+    Html:"pyton",
+    css:"React",
+    jav:"javascript"
 
-export default Data;
+},
+{
+    id:3,
+    img:<img src={the} alt=""></img>,
+    name:"ACCOUNT",
+    book:"New",
+    
+    sen:"junior frontend Developer",
+    day:"2day ago ",
+    full:"part time",
+    usa:"usa only",
+    front:"frontend",
+    senior:"junior",
+    Html:"React ",
+    css:"sas",
+    jav:"javascript"
+
+},
+{
+    id:5,
+    img:<img src={five} alt=""></img>,
+    name:"photosnap",
+
+    sen:"junior frontend Developer",
+    day:"1day ago ",
+    full:"full time",
+    usa:"word wide",
+    front:"frontend",
+    senior:"senior",
+    Html:"Html",
+    css:"sass",
+    jav:"Ruby"
+
+    
+},
+{
+    id:6,
+    img:<img src={six} alt=""></img>,
+
+    cat:"FEATURES",
+    sen:"junior frontend Developer",
+    day:"2wk ago ",
+    full:"full time",
+    usa:"uk only",
+    front:"backend",
+    senior:"junior",
+    Html:"ruby",
+    css:"bolt",
+    jav:"javascript"
+
+    
+},
+{
+    id:7,
+    img:<img src={seven} alt=""></img>,
+    name:"shorly",
+
+    sen:"junior frontend Developer",
+    day:"2wk ago ",
+    full:"full time",
+    usa:"usa only",
+    front:"frontend",
+    senior:"senior",
+    Html:"Html",
+    css:"javascript",
+    jav:"sass"
+
+    
+},
+
+{
+    id:8,
+    img:<img src={eig} alt=""></img>,
+    name:"insure",
+    sen:"junior frontend Developer",
+    day:"2wk ago ",
+    full:"full time",
+    usa:"usa only",
+    front:"frontend",
+    senior:"senior",
+    Html:"vux",
+    css:"javascript",
+    jav:"sax"
+
+    
+},
+{
+    id:9,
+    img:<img src={nig} alt=""></img>,
+    name:"Every co",
+
+    sen:"full frontend Developer",
+    day:"3day ago ",
+    full:"full time",
+    usa:"usa only",
+    front:"frontend",
+    senior:"senior",
+    Html:"Html",
+    css:"css",
+    jav:"javascript"
+
+    
+},
+{
+    id:1,
+    img:<img src={ten} alt=""></img>,
+    name:"the air filter netwok",
+
+    sen:"Senior frontend Developer",
+    day:"1day ago ",
+    full:"full time",
+    usa:"usa only",
+    front:"frontend",
+    senior:"senior",
+    Html:"Html",
+    css:"css",
+    jav:"javascript"
+
+    
+},
+]
+
+export default data;
