5dacb637ce74fffc01a0c2dddd5695b3e5241f92
Ajewole Gabriel <Ajewole.G@bullseye.local>
2023-06-10 04:44:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import bg from './image/bg-header-desktop.svg'
+import Data from './data';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [lo,setlo]=useState('')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <div className='back' >
+    <div  >
+    <img className='head'  src= {bg} alt=""/>
+
+
+    <nav>
+      <div className='pal' >
+        <section>{Data[0].img}</section>
+        
+        <div className='cover' >
+           <section className='pa' >
+          <div>{Data.name}</div>
+          <div><button className='pb' >{Data[0].cat}</button></div>
+          <div><button className='pbb' > {Data[0].name} </button> </div>
+           </section>
+
+          <p  className='sen'  ><b> senior Frontend Devaloper </b></p>
+          <p className='se' > 1d ago     <span className='span' > . Part Time   </span> <span className='span' > . Usa only    </span>     </p>
+        </div>
+
+        <section className='lastbutton' >
+        <button className='button' >Frontend</button>
+        <button className='button' >senior</button>
+        <button className='button' >HTML</button>
+        <button className='button' >CSS</button>
+        <button className='button' >Javascript</button>
+
+        </section>
+      </div>
+    </nav>
+      
     </div>
+
+  </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,22 @@
+import one from './image/photosnap.svg'
+
+const Data =[{
+    id:1,
+    img:<img src={one} alt=""></img>,
+    name:"photosnap",
+    book:"New",
+    cat:"FEATURES",
+    sen:"Senior frontend Developer",
+    day:"1day ago ",
+    full:"full time",
+    usa:"usa only",
+    front:"frontend",
+    senior:"senior",
+    Html:"Html",
+    css:"css",
+    jav:"javascript"
+
+    
+}]
+
+export default Data;
